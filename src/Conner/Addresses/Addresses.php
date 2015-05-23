d1dedc395146f977f8d07ee4b6b7699b688db509
mikenolimits <empathynyc@gmail.com>
2015-05-23 17:32:22 -0400

    fix setFlag

--- src/Conner/Addresses/Addresses.php
+++ src/Conner/Addresses/Addresses.php
@@ -169,7 +169,9 @@ class Addresses {
 	private function getFlag($flag, $userId=null) {
 		$userId = $userId ?: self::userId();
 
-		if(empty($userId)) { return null; }
+		if(empty($userId)) {
+            return null;
+        }
 		
 		return Address::where('user_id', '=', $userId)
 			->where('is_'.$flag, true)
